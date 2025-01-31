dd951b13bc9d776ec2877b5b1281dfbceba2833e
sethjfischer <seth@itsgeek2me.net>
2025-01-31 10:01:35 -0500

    emulator cleanup update

--- emulator.sh
+++ emulator.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Cleanup old firebase-export* folders
+echo "Cleaning up old firebase-export* folders..."
+rm -rf ./firebase-export*
+
+# Kill any processes using the emulator ports
 ports=(9099 4000 5001 8080 5025 9199 8085)
 for port in "${ports[@]}"; do
   pid=$(lsof -ti :$port)
@@ -10,9 +15,13 @@ for port in "${ports[@]}"; do
     kill -9 $pid
   fi
 done
+
+# Ensure firebase_data directory exists
 DIR="./firebase_data"
 if [ ! -d "$DIR" ]; then
  cp -r ./firebase_data_emulator_seed ./firebase_data
 fi
+
+# Start Firebase emulators
 export NODE_ENV=development
 firebase emulators:start --import ./firebase_data --export-on-exit
