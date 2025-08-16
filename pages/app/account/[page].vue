d5b3b7653f8641baebcc70207bf65cc44a768a9d
sethjfischer <seth@itsgeek2me.net>
2025-08-16 00:15:21 -0600

    updates

--- pages/app/account/[page].vue
+++ pages/app/account/[page].vue
@@ -56,89 +56,28 @@ const page = computed(() => {
 <template>
   <div
     v-if="edgeGlobal.edgeState.organizationDocPath"
-    class="w-full max-w-7xl mx-auto  bg-white rounded-[9.96px] shadow-md px-6"
   >
     <edge-organization-settings
       v-if="page === 'organization-settings'"
       :subscribe-options="subscribeOptions"
       :form-schema="orgSchema"
       :org-fields="orgFields"
-      class="bg-white"
-    >
-      <template #header>
-        <Teleport to="#page-header">
-          <edge-shad-breadcrumbs
-            :items="[
-              { text: 'Settings', to: '/app/dashboard/my-profile' },
-              { text: 'Organization', to: '' },
-            ]"
-          />
-        </Teleport>
-      </template>
-    </edge-organization-settings>
+    />
     <edge-my-account
       v-if="page === 'my-account'"
-      class="bg-white"
-    >
-      <template #header>
-        <Teleport to="#page-header">
-          <edge-shad-breadcrumbs
-            :items="[
-              { text: 'Settings', to: '/app/dashboard/my-profile' },
-              { text: 'My Account', to: '' },
-            ]"
-          />
-        </Teleport>
-      </template>
-    </edge-my-account>
+    />
     <edge-my-profile
       v-if="page === 'my-profile'"
       :form-schema="metaSchema"
       :meta-fields="metaFields"
-      class="bg-white"
-    >
-      <template #header>
-        <Teleport to="#page-header">
-          <edge-shad-breadcrumbs
-            :items="[
-              { text: 'Settings', to: '/app/dashboard/my-profile' },
-              { text: 'My Profile', to: '' },
-            ]"
-          />
-        </Teleport>
-      </template>
-    </edge-my-profile>
+    />
     <edge-organization-members
       v-if="page === 'organization-members'"
-      class="bg-white"
-    >
-      <template #header>
-        <Teleport to="#page-header">
-          <edge-shad-breadcrumbs
-            :items="[
-              { text: 'Settings', to: '/app/dashboard/my-profile' },
-              { text: 'Users', to: '' },
-            ]"
-          />
-        </Teleport>
-      </template>
-    </edge-organization-members>
+    />
     <edge-my-organizations
       v-if="page === 'my-organizations'"
       :registration-code="config.public.registrationCode"
-      class="bg-white"
-    >
-      <template #header>
-        <Teleport to="#page-header">
-          <edge-shad-breadcrumbs
-            :items="[
-              { text: 'Settings', to: '/app/dashboard/my-profile' },
-              { text: 'My Organizations', to: '' },
-            ]"
-          />
-        </Teleport>
-      </template>
-    </edge-my-organizations>
+    />
     <edge-billing
       v-if="page === 'subscription'"
     />
