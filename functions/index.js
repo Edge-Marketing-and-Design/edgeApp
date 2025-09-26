ba59449b281a815837537ed571301c866c1cfc32
sethjfischer <seth@itsgeek2me.net>
2025-09-25 21:12:35 -0400

    wip

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config({ path: process.env.NODE_ENV === 'production' ? '.env.prod' : '.env.dev' })
 
 exports.stripe = require('./stripe')
+// exports.cms = require('./cms')
 
 // START @edge/firebase functions
 exports.edgeFirebase = require('./edgeFirebase')
